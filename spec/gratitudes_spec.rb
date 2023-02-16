33f291169adbb1591e5359a7f67d7e2aa14d219f
Fiona Wiggins <fiona@fionawiggins.co.uk>
2023-02-16 16:14:11 +0000

    Present wrapping

--- spec/gratitudes_spec.rb
+++ spec/gratitudes_spec.rb
@@ -3,7 +3,7 @@ require 'gratitudes'
 RSpec.describe Gratitudes do
   it "Lists what the user is grateful for" do
     gratitudes = Gratitudes.new()
-    gratitudes.add("capers") # Makes it fail nicely.
+    gratitudes.add("capers") # Makes the code fail nicely.
     result = gratitudes.format()
     expect(result).to eq "Be grateful for: laughter"
     # The question is how to test for the +=
